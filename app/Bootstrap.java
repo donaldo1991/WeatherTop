c12d2b7347d9b3903a77265db0600d39c0f86432
Donal Murphy <20100051@mail.wit.ie>
2022-05-27 17:02:18 +0100

    Prepare application for deployment with Heroku

--- app/Bootstrap.java
+++ app/Bootstrap.java
@@ -1,13 +1,19 @@
-import play.jobs.Job;
-import play.jobs.OnApplicationStart;
-import play.test.Fixtures;
+import java.util.List;
+
+import play.*;
+import play.jobs.*;
+import play.test.*;
+
+import models.*;
 
 @OnApplicationStart
 public class Bootstrap extends Job
 {
     public void doJob()
     {
-        Fixtures.loadModels("readings.yml");
+        if (Member.count() == 0)
+        {
+            Fixtures.loadModels("readings.yml");
+        }
     }
 }
-
